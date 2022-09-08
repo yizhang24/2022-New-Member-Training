79a01f6d26df28eb0d7c3b65505097c175496ea4
Yi Zhang <yi.zhang.yzfl@gmail.com>
2022-09-07 20:05:07 -0700

    Initial Commit

---
+++ Yi/Quadratic.java
@@ -0,0 +1,22 @@
+public class Quadratic {
+    public static void main(String[] args) {
+        // double a = -10;
+        // double b = 5;
+        // double c = 1;
+
+        // double bsquared = (b * b);
+
+        // double discriminant = bsquared - (4 * a * c);
+
+        // if (discriminant == 0) {
+        //     System.out.println("One Root!");
+        // } else if (discriminant > 0) {
+        //     System.out.println("Two Roots");
+        // } else {
+        //     System.out.println("No Roots!");
+        // }
+
+        System.out.print("Red");
+        System.out.print(" and Blue");
+    }
+}
