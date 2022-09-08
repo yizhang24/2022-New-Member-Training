a858993b4e5fc055fee61eafa64be3cdcf6b2166
zwang259 <wzoe1463@gmail.com>
2022-09-07 20:23:07 -0700

    Add QuadraticFile

---
+++ Zoe/quadracticpractice.java
@@ -0,0 +1,31 @@
+public class quadracticpractice {
+    public static void main(String[] args) {
+        double a = 2;
+        double b = 6; 
+        double c = 2; 
+        double root1 = (b * b - 4 * a * c);
+        if (a >= 0); {
+            System.out.println("Open Upward"); 
+        } /*else {
+            
+            System.out.println("Open Downward"); 
+        } */
+        if (root1 >= 0){
+            System.out.println("Two Root Exists");
+        }
+        else if (root1 == 0 ) {
+            System.out.println("One Root Exists");
+        }
+            
+        else {
+            System.out.println("No Real Roots Exists");
+        }
+
+        
+
+        }
+
+        }
+
+    
+
