c21a1855135a9073ca610eb18e067de1ac272c97
kaylaSLee <klee33727@gmail.com>
2022-09-29 19:25:37 -0700

    Add classes practice

---
+++ Kayla/classes2/Main.java
@@ -0,0 +1,12 @@
+package classes2;
+
+public class Main {
+    public static void main(String[] args) { 
+
+        flywheel flywheel1 = new flywheel(); 
+
+        flywheel1.setSpeed(9.8);
+
+        System.out.println(flywheel1.getSpeed() + " m/s");
+    }
+}
